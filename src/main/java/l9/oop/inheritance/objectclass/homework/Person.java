b1a31407866ff23e890ff8138f280f06baa1561d
İlkin <97682974+ilkinsdqv@users.noreply.github.com>
2026-04-24 20:03:35 +0400

    lesson9

---
+++ src/main/java/l9/oop/inheritance/objectclass/homework/Person.java
@@ -0,0 +1,6 @@
+package l9.oop.inheritance.objectclass.homework;
+
+public class Person {
+    int age;
+    String name;
+}
---
+++ src/main/java/l9/oop/inheritance/objectclass/homework/Worker.java
@@ -0,0 +1,5 @@
+package l9.oop.inheritance.objectclass.homework;
+
+public class Worker extends Person {
+
+}
